474af90dcf77f39c616a291f2e0b7c1e0231c366
Sbyner <tmmscrltt@gmail.com>
2021-12-02 17:09:08 +0100

    Add basic parallelization formalization transformation

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ if __name__ == '__main__':
     maude.load('patt.maude')
 
     pa = maude.getModule("RULES")
-    first_to_parse = 'A 1 . A 2 . A 3 . ( A 5 . A 6 || A 4 ) . A 8 . nil =|= e'
+    first_to_parse = 'A 1 . ( A 3 || A 4 || A 5 ) . A 8 . nil =|= e # 0'
     anotherPa = pa.parseTerm(first_to_parse)
-    length_to_parse = 'length(A 1 . A 2 . A 5 || A 4 . nil)'
+    length_to_parse = 'length(nil)'
     length = pa.parseTerm(length_to_parse)
-    ln_to_parse = 'A 1 . A 2 . nil =|= e'
+    ln_to_parse = 'A 1 . A 2 . nil =|= e # 0'
     ln = pa.parseTerm(ln_to_parse)
     anotherPa.rewrite()
     length.reduce()
     ln.rewrite()
-    print(length_to_parse, '\n', length)
-    print(first_to_parse , '\n', anotherPa)
-    print(ln_to_parse, '\n', ln)
+    print(length_to_parse, '\n', length, '\n')
+    print(first_to_parse , '\n', anotherPa, '\n')
+    print(ln_to_parse, '\n', ln, '\n')
