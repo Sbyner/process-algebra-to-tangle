6c6fc9672708ca04120b7c2b13771451f6b7e377
Sbyner <tmmscrltt@gmail.com>
2021-12-16 16:57:46 +0100

    Integration with Michi's utility

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python3
 import maude
+import sys
+
+def init_arg(sasso):
+    return f"{sasso} =|= e # 0"
+
+def test_conversion(sasso):
+    pa = maude.getModule("RULES")
+    width = 17
+    print(f"\n{'To convert:':<{width}} {sasso}")
+    toMaude = pa.parseTerm(sasso)
+    toMaude.reduce()
+    print(f"{'Reduce:':<{width}} {toMaude}")
+    toMaude = pa.parseTerm(sasso)
+    toMaude.rewrite()
+    print(f"{'Rewrite:':<{width}} {toMaude}")
+    toMaude = pa.parseTerm(f"getTangle({str(toMaude)})")
+    toMaude.reduce()
+    print(f"{'Rewrite - reduce:':<{width}} {toMaude}")
+
+def convert(sasso):
+    pa = maude.getModule("RULES")
+    toMaude = pa.parseTerm(sasso)
+    toMaude.rewrite()
+    toMaude = pa.parseTerm(f"getTangle({str(toMaude)})")
+    toMaude.reduce()
+    return str(toMaude)
 
 if __name__ == '__main__':
     maude.init()
-
     maude.load('patt.maude')
 
-    pa = maude.getModule("RULES")
-    first_to_parse = 'A 1 . ( A 3 || A 4 || A 5 ) . A 8 . nil =|= e # 0'
-    anotherPa = pa.parseTerm(first_to_parse)
-    length_to_parse = 'length(nil)'
-    length = pa.parseTerm(length_to_parse)
-    ln_to_parse = 'A 1 . A 2 . nil =|= e # 0'
-    ln = pa.parseTerm(ln_to_parse)
-    anotherPa.rewrite()
-    length.reduce()
-    ln.rewrite()
-    print(length_to_parse, '\n', length, '\n')
-    print(first_to_parse , '\n', anotherPa, '\n')
-    print(ln_to_parse, '\n', ln, '\n')
+
+    if len(sys.argv) <= 1:
+        print("Not enough args")
+        exit(1)
+    
+    out: str = convert(init_arg(sys.argv[1]))
+    arr = [s.strip() for s in out.split('~')]
+    # arr = [(i,i2) for i, v in enumerate(arr)]
+    outlist = []
+    for i,v in enumerate(arr):
+        for k,z in enumerate(arr):
+            if v == z and i<k:
+                outlist.append((i,k))
+                
+    print(','.join([f"{i}:{k}" for i,k in outlist]))
